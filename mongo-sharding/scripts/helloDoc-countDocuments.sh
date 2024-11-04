2d181512c5b8e0302eff9bc8231b8600602377d2
a.bardanov <a.bardanov@yablochcontech.com>
2024-11-04 16:52:37 +0300

    task2: настроил шардирование

---
+++ mongo-sharding/scripts/helloDoc-countDocuments.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Всего документов в базе данных: "
+docker compose exec -T mongos_router mongosh --port 27020 --quiet << 'EOF'
+use somedb;
+db.helloDoc.countDocuments(); 
+EOF
+echo ""
+
+echo "Шард №1 содержит: "
+docker compose exec -T shard1 mongosh --port 27018 --quiet <<EOF
+use somedb
+db.helloDoc.countDocuments()
+EOF
+echo ""
+
+echo "Шард №2 содержит: "
+docker compose exec -T shard2 mongosh --port 27019 --quiet <<EOF
+use somedb
+db.helloDoc.countDocuments()
+EOF
+echo ""
